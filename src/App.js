59edf24f77a8fa39f68985033d09f9a90ff8fc51
Manuel Valles <manukempo@gmail.com>
2020-09-04 18:03:09 +0100

    Separate App into Modules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Pet from './Pet';
 
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ]);
-};
 const App = () => {
   return React.createElement('div', { id: 'something-important' }, [
     React.createElement('h1', {}, 'Adopt Me!'),
