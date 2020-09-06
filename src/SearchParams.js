9b0d5946017d50407335c03221fbe22f91e632b7
Manuel Valles <manukempo@gmail.com>
2020-09-06 14:21:32 +0100

    One-Way Data Flow

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
@@ -49,6 +50,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
