8cccb2b275fde80e5d86211ae2f8aa022fac82a5
Manuel Valles <manukempo@gmail.com>
2020-09-07 18:44:50 +0100

    Reformat the Pet Component

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,15 @@
 import React from 'react';
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  const hero = media.length ? media[0].small : 'http://placecorgi.com/300/300';
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
