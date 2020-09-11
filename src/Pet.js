bcd5c899ca1243040df994ed2a7212ee60a83311
Manuel Valles <manukempo@gmail.com>
2020-09-11 18:30:42 +0100

    Fixed bug: href to Link to avoid reloading the page and destroying the DOM

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from '@reach/router';
+
 export default function Pet({ name, animal, breed, media, location, id }) {
   const hero = media.length ? media[0].small : 'http://placecorgi.com/300/300';
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -10,6 +12,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
